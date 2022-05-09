ec18bae42e6a68dd61a94f7f2c4a8ea47b653c8b
447441478 <39853351+447441478@users.noreply.github.com>
2022-05-09 10:43:42 +0800

    reverseList

--- src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
+++ src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -75,18 +75,3 @@ public class RemoveNthNodeFromEndOfList {
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-
-    @Override
-    public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next=" + next +
-                '}';
-    }
-}
